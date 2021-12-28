3a9aa974236dd55e145ed0e7661858d9e042040a
Joel Bladt <joel@bladt.de>
2021-12-28 23:40:30 +0100

    Vue Router classes and Stylesheet adjusted

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav class="flex sm:justify-center space-x-4 py-6">
+    <router-link
+        class="rounded-lg px-3 text-gray-700 font-medium hover:bg-gray-100 hover:text-gray-900"
+        exact-active-class="text-green-400"
+        to="/"
+    >
+      Home
+    </router-link>
+    <router-link
+        class="rounded-lg px-3 text-gray-700 font-medium hover:bg-gray-100 hover:text-gray-900"
+        exact-active-class="text-green-400"
+        to="/about"
+    >
+      About
+    </router-link>
+  </nav>
   <router-view/>
 </template>
 
@@ -13,18 +25,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
