265f41fe008ed1c84b7cf91ac7ed5f103e033556
joelbladt <joel@bladt.de>
2022-08-18 19:33:25 +0200

    Add font Roboto

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex sm:justify-center space-x-4 py-6">
+  <!-- nav class="flex sm:justify-center space-x-4 py-6">
     <router-link
         class="rounded-lg px-3 text-gray-700 font-medium hover:bg-gray-200 hover:text-gray-900"
         exact-active-class="text-green-400"
@@ -14,16 +14,17 @@
     >
       About
     </router-link>
-  </nav>
-  <router-view/>
+  </nav -->
+  <router-view />
 </template>
 
 <style lang="scss">
+@import '../node_modules/typeface-roboto/index.css';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #345;
 }
 </style>
