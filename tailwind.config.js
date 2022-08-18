ce9fb5cd1e7f77ff38cf798997434b6ecb9e6117
joelbladt <joel@bladt.de>
2022-08-18 19:31:26 +0200

    Add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -8,5 +10,17 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': {
+          fontSize: '8vw',
+          letterSpacing: '-5px',
+          fontWeight: theme('fontWeight.bold')
+        },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ],
 }
