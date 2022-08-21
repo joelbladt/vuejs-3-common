8a9df77c4fb649c0e3235b23fb2c230525f5c828
joelbladt <joel@bladt.de>
2022-08-21 05:29:11 +0200

    upgrade guide: configure content sources & remove dark mode configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
